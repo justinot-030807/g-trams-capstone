3ef45ed78278211fdeae86f056bde3f78a62eadd
unknown <lachica.justine@marsu.edu.ph>
2026-07-13 00:07:51 +0800

    fix: updated nodemailer config to prevent ETIMEDOUT

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,12 +1,19 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
+    // TINA-TARGET NATIN MISMO ANG SMTP SERVER NG GMAIL
     const transporter = nodemailer.createTransport({
-        service: 'Gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // true kapag port 465
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
+        tls: {
+            // Ito ang sikreto para hindi i-reject ng cloud server ang connection
+            rejectUnauthorized: false 
+        }
     });
 
     const mailOptions = {
